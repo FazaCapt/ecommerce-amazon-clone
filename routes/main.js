fa49220b06cc2f0fad9cc910a1256b611badfa9b
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-06-11 02:47:28 +0700

    Products route

--- routes/main.js
+++ routes/main.js
@@ -9,10 +9,41 @@ router.get('/about', function(req, res) {
     res.render('main/about');
 });
 
-router.get('/users', function(req, res) {
-    User.find({}, function(err, users) {
-        res.json(users);
+// router.get('/users', function(req, res) {
+//     User.find({}, function(err, users) {
+//         res.json(users);
+//     });
+// });  
+
+router.get('/products/:id', function(req, res, next) {
+    Product
+    .find({ category: req.params.id})
+    .populate('category')
+    .exec(function(err, products) {
+        if(err) return next(err);
+        res.render('main/category', {
+            products: products
+        });
     });
-});  
+});
+
+// products.category.name
+
+// {
+//     category: 12312132,
+//     name: harry potter,
+//     price: 12345,
+//     image: gravatar.com/link
+// }
+
+// {
+//     category:{
+//         _id: 1231231,
+//         name: foods
+//     }
+//     name: harry potter,
+//     price: 12345,
+//     image: gravatar.com/link
+// }
 
 module.exports = router;
